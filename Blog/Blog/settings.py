a8a10d2d6c285f7e72c21e8d52da049b8c407edc
vedant bharti <39922381+vedantbharti@users.noreply.github.com>
2020-04-15 01:28:21 +0530

    Update settings.py

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mediumeditor',
+    'bootstrap3',
+    'crispy_forms',
     'blog_app',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
